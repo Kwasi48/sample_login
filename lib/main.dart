137351cb8ab06d15045b52826e78d5cd93896673
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-10-12 03:37:35 +0000

    still trying ti initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:sample_login/view/signup.dart';
 
-void main() {
+void main() async  {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
