a08e6b0b9a6c30165baec5bcda033382cfea9485
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-10-19 21:04:25 +0000

    adding firebase authentication

--- lib/controller/shared/authentication.dart
+++ lib/controller/shared/authentication.dart
@@ -7,8 +7,14 @@ class Authentication{
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   Future<String> login(String email, String password) async {
-    UserCredential authCredential = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
-    User? user = authCredential.user;
+    UserCredential userCredential = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+    User? user = userCredential.user;
+    return user!.uid;
+  }
+
+  Future<String> SignUp (String email,String password) async {
+    UserCredential userCredential = await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+    User? user = userCredential.user;
     return user!.uid;
   }
 }
